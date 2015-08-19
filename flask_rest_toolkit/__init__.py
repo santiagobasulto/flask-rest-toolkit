1ca9a0179ee2c01db51caa5b1d2a90b9c616e024
Santiago Basulto <santiago.basulto@gmail.com>
2015-08-19 09:31:44 -0300

    High level interface

--- flask_rest_toolkit/__init__.py
+++ flask_rest_toolkit/__init__.py
@@ -4,3 +4,7 @@ __author__ = 'Santiago Basulto'
 __license__ = 'MIT'
 __description__ = ("A set of tools to create simple Flask REST "
                    "web services and APIs")
+
+
+from .api import Api
+from .endpoint import ApiEndpoint
