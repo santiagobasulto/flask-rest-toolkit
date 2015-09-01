8ca5a38d0a0c23583e5c81f1141c2118ec85f3d3
Santiago Basulto <santiago.basulto@gmail.com>
2015-09-01 16:35:29 -0300

    Upgraded version to 0.0.2

--- flask_rest_toolkit/__init__.py
+++ flask_rest_toolkit/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'flask_rest_toolkit'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Santiago Basulto'
 __license__ = 'MIT'
 __description__ = ("A set of tools to create simple Flask REST "
